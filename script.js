c48d5089a22828a258c33d7dc528ff35fcca03d1
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 01:15:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+function decryptMessage(str , shift){
+    let newStr = "";
+    for(let i = 0 ; i< str.length ; i++){
+        if(str.charAt(i)==" "){
+            newStr+=" "; 
+            continue;
+        } 
+        newStr+=String.fromCharCode((((((str.charAt(i).charCodeAt(0))+shift)-97)%26)+97))
+    }
+    return newStr;
+}
+
+
+function sortMessages(arr){
+    const realmsg=[]
+    arr.sort((a,b)=>{
+        return (new Date(a['date']) - new Date(b['date']))    
+    })
+    for(let i = 0 ; i< arr.length ; i++){
+         realmsg.push(arr[i])
+    }
+    return realmsg;
+
+}
+
+function storeMessages(arr){
+    localStorage.setItem('messages',arr)
+}
